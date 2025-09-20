8dce2970ada001830463d0dd77b68bbebd08d8ba
thanesh-45 <thanesh8270@gmail.com>
2025-09-20 23:28:34 +0530

    Update config.py

--- ancient_artisans/config.py
+++ ancient_artisans/config.py
@@ -6,10 +6,13 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'gG9Yt#7eZ@1wR$Lp8*BvQk2nX!mF^3sJ')
-    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '8194')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'artisan')
+     host="dpg-d37dv6mr433s73em4si0-a.oregon-postgres.render.com",
+      database="heritiq",
+      user="heritiq_user",
+      password="0WPsDiNnqavzeyDGtgInjEXXG9yzc5WI",
+      port=5432,
+      sslmode="require"
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'static/uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB
+
     GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY', 'AIzaSyC2PhmUXUxTTQnpqSVl1jhVu8nEPlTpji4')
